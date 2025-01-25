5494dc1abe87a5ac66ecc4cfb2b281c46a4af6cb
SolitaryAnimal <155461314+SolitaryAnimal@users.noreply.github.com>
2025-01-25 13:17:32 +0800

    fix range bug

--- Runtime/MaterialCuverProperty/CuvreToTexture.cs
+++ Runtime/MaterialCuverProperty/CuvreToTexture.cs
@@ -8,7 +8,9 @@ namespace Sanimal
         public static void UpdateTextureFromCurveCPU(AnimationCurve curve, Texture2D texture)
         {
             var width = texture.width;
-            texture.SetPixels(Enumerable.Range(0, width).Select(x => new Color(curve.Evaluate((float)x / width), 0, 0)).ToArray());
+            var range = Enumerable.Range(0, width);
+            width--;
+            texture.SetPixels(range.Select(x => new Color(curve.Evaluate((float)x / width), 0, 0)).ToArray());
             texture.Apply();
         }
     }
